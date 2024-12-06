9ebf4251385aa33c79f9d0931e40a0805e9e49f7
mearforreal <meirerbol@gmail.com>
2024-12-07 03:37:14 +0500

    as

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,32 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {
+  useAuthenticator,
+  withAuthenticator,
+} from "@aws-amplify/ui-react-native";
+
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+import { Amplify } from "aws-amplify";
+import amplifyconfig from "../src/amplifyconfiguration.json";
+Amplify.configure(amplifyconfig);
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
+function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -28,7 +40,7 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
@@ -37,3 +49,5 @@ export default function RootLayout() {
     </ThemeProvider>
   );
 }
+
+export default withAuthenticator(RootLayout);
